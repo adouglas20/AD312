1de206c56b64eb2f32af83555f52ff1da6338431
Andy Douglas <andydouglas6416@gmail.com>
2026-04-10 10:39:38 -0700

    added state_management

---
+++ state_management/src/Counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import './Counter.css'
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="container">
+            <p>{count}</p>
+            <button className="control-button" onClick={() => setCount(count + 1)}>
+                Increment
+            </button>
+
+            <button className="control-button" onClick={() => setTimeout(() => setCount(count + 1), 2000)}>
+                Increment after delay
+            </button>
+
+            <button className="control-button" onClick={() => setCount(count + 2)}>
+                Increment Twice
+            </button>
+
+            <button className="control-button" onClick={() => {
+                setCount(prev => prev + 1)
+                setCount(prev => prev + 1)
+
+            }}>
+                CorrectIncrement Twice
+            </button>
+        </div>
+    );
+}
+
+export default Counter
+
+
